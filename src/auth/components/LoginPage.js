cce6131cf268abe501f0e8b7fb25886d5dd35cf3
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-05-07 10:53:27 -0300

    SignupPage added

--- src/auth/components/LoginPage.js
+++ src/auth/components/LoginPage.js
@@ -36,11 +36,12 @@ class Login extends React.Component {
 
     componentDidMount() {
         var html = document.getElementsByTagName('html')[0];
-        html.setAttribute('class', 'authentication');
+        html.classList.add('authentication');
     }
 
     componentWillUnmount() {
-        $('html').removeClass('authentication');
+        var html = document.getElementsByTagName('html')[0];        
+        html.classList.remove('authentication');
     }
 
     handleBack(e) {
@@ -49,65 +50,65 @@ class Login extends React.Component {
         this.props.router.goBack();
     }
 
-  handleSubmit(e) {
-      e.preventDefault();
-      this.props.doLogin(this.state).then(
-          ({data}) => {
-              console.log(data);
-          },
-          (data) => {
-              console.error(data);
-          }
-      );
-  }
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.doLogin(this.state).then(
+            ({data}) => {
+                console.log(data);
+            },
+            (data) => {
+                console.error(data);
+            }
+        );
+    }
 
-  handleChange(e) {
-      this.setState({ [e.target.name]: e.target.value });
-  }
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
 
-  getPath(path) {
-    return 'oi';
-  }
+    getPath() {
+        return '-';
+    }
 
-  render() {
-      return (
-          <div id='auth-container' className='login'>
-              <div id='auth-row'>
-                  <div id='auth-cell'>
-                      <Grid>
-                          <Row>
-                              <Col sm={4} smOffset={4} xs={10} xsOffset={1} collapseLeft collapseRight>
-                                  <PanelContainer controls={false}>
-                                      <Panel>
-                                      <PanelBody style={{padding: 0}}>
-                                          <div className='text-center bg-darkblue fg-white'>
-                                          <h3 style={{margin: 0, padding: 25}}>Sign in to Rubix</h3>
-                                          </div>
-                                          <div className='bg-hoverblue fg-black50 text-center' style={{padding: 12.5}}>
-                                          <div>You need to sign in for those awesome features</div>
-                                          <div style={{marginTop: 12.5, marginBottom: 12.5}}>
-                                              <Button id='facebook-btn' lg bsStyle='darkblue' type='submit' onClick={this.back}>
-                                              <Icon glyph='icon-fontello-facebook' />
-                                              <span>Sign in <span className='hidden-xs'>with facebook</span></span>
-                                              </Button>
-                                          </div>
-                                          <div>
-                                              <a id='twitter-link' href='#' onClick={this.back}><Icon glyph='icon-fontello-twitter' /><span> or with twitter</span></a>
-                                          </div>
-                                          </div>
-                                          <div>
-                                          <div className='text-center' style={{padding: 12.5}}>
-                                              or use your Rubix account
-                                          </div>
-                                          <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
-                                              <Form onSubmit={this.back}>
+    render() {
+        return (
+            <div id='auth-container' className='login'>
+                <div id='auth-row'>
+                    <div id='auth-cell'>
+                        <Grid>
+                            <Row>
+                                <Col sm={4} smOffset={4} xs={10} xsOffset={1} collapseLeft collapseRight>
+                                    <PanelContainer controls={false}>
+                                        <Panel>
+                                        <PanelBody style={{padding: 0}}>
+                                            <div className='text-center bg-darkblue fg-white'>
+                                            <h3 style={{margin: 0, padding: 25}}>Sign in to Rubix</h3>
+                                            </div>
+                                            <div className='bg-hoverblue fg-black50 text-center' style={{padding: 12.5}}>
+                                            <div>You need to sign in for those awesome features</div>
+                                            <div style={{marginTop: 12.5, marginBottom: 12.5}}>
+                                                <Button id='facebook-btn' lg bsStyle='darkblue' type='submit' onClick={this.back}>
+                                                <Icon glyph='icon-fontello-facebook' />
+                                                <span>Sign in <span className='hidden-xs'>with facebook</span></span>
+                                                </Button>
+                                            </div>
+                                            <div>
+                                                <a id='twitter-link' href='#' onClick={this.back}><Icon glyph='icon-fontello-twitter' /><span> or with twitter</span></a>
+                                            </div>
+                                            </div>
+                                            <div>
+                                            <div className='text-center' style={{padding: 12.5}}>
+                                                or use your Rubix account
+                                            </div>
+                                            <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
+                                                <Form onSubmit={this.back}>
 
-                                              <FormGroup controlId='emailaddress'>
-                                                  <InputGroup bsSize='large'>
-                                                  <InputGroup.Addon>
-                                                      <Icon glyph='icon-fontello-mail' />
-                                                  </InputGroup.Addon>
-                                                  <FormControl 
+                                                <FormGroup controlId='emailaddress'>
+                                                    <InputGroup bsSize='large'>
+                                                    <InputGroup.Addon>
+                                                        <Icon glyph='icon-fontello-mail' />
+                                                    </InputGroup.Addon>
+                                                    <FormControl 
                                                     autoFocus
                                                     className='border-focus-blue'  
                                                     type='email'                                                     
@@ -115,51 +116,51 @@ class Login extends React.Component {
                                                     name='email'
                                                     onChange={this.handleChange}
                                                     value={this.state.email} />
-                                                  </InputGroup>
-                                              </FormGroup>
+                                                    </InputGroup>
+                                                </FormGroup>
 
-                                              <FormGroup controlId='password'>
-                                                  <InputGroup bsSize='large'>
-                                                  <InputGroup.Addon>
-                                                      <Icon glyph='icon-fontello-key' />
-                                                  </InputGroup.Addon>
-                                                  <FormControl 
+                                                <FormGroup controlId='password'>
+                                                    <InputGroup bsSize='large'>
+                                                    <InputGroup.Addon>
+                                                        <Icon glyph='icon-fontello-key' />
+                                                    </InputGroup.Addon>
+                                                    <FormControl 
                                                     className='border-focus-blue' 
                                                     type='password' 
                                                     placeholder='password' 
                                                     name='password' 
                                                     onChange={this.handleChange}
                                                     value={this.state.password} />
-                                                  </InputGroup>
-                                              </FormGroup>
+                                                    </InputGroup>
+                                                </FormGroup>
 
-                                              <FormGroup>
-                                                  <Grid>
-                                                  <Row>
-                                                      <Col xs={6} collapseLeft collapseRight style={{paddingTop: 10}}>
-                                                      <Link to={this.getPath('signup')}>Create a Rubix account</Link>
-                                                      </Col>
-                                                      <Col xs={6} collapseLeft collapseRight className='text-right'>
-                                                      <Button outlined lg type='submit' bsStyle='blue' onClick={this.handleSubmit}>Login</Button>
-                                                      </Col>
-                                                  </Row>
-                                                  </Grid>
-                                              </FormGroup>
+                                                <FormGroup>
+                                                    <Grid>
+                                                    <Row>
+                                                        <Col xs={6} collapseLeft collapseRight style={{paddingTop: 10}}>
+                                                        <Link to={this.getPath('signup')}>Create a Rubix account</Link>
+                                                        </Col>
+                                                        <Col xs={6} collapseLeft collapseRight className='text-right'>
+                                                        <Button outlined lg type='submit' bsStyle='blue' onClick={this.handleSubmit}>Login</Button>
+                                                        </Col>
+                                                    </Row>
+                                                    </Grid>
+                                                </FormGroup>
 
-                                              </Form>
-                                          </div>
-                                          </div>
-                                      </PanelBody>
-                                      </Panel>
-                                  </PanelContainer>
-                                  </Col>
-                          </Row>
-                      </Grid>
-                  </div>
-              </div>
-          </div>
-      )
-  }  
+                                                </Form>
+                                            </div>
+                                            </div>
+                                        </PanelBody>
+                                        </Panel>
+                                    </PanelContainer>
+                                    </Col>
+                            </Row>
+                        </Grid>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 Login.propTypes = {
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import { IndexRoute, Route, Router } from 'react-router';
 import { App, Dashboard } from './app';
 import Home from './common/components/Home';
 import Login from './auth/components/LoginPage';
+import Signup from './auth/components/SignupPage';
 
 export default (  
     <Router component={App}>
       <Route path='/login' component={Login} />
+      <Route path='/signup' component={Signup} />
 
       <Route path='/' component={Dashboard} >
         <IndexRoute component={Home} />
