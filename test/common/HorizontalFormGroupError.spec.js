a3d7242f8cec5cf2a7482d80f844106741aa00cf
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-06-05 19:16:53 -0300

    Added tests to HorizontalFormGroupError

---
+++ test/common/HorizontalFormGroupError.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import { FormControl } from '@sketchpixy/rubix';
+
+import HorizontalFormGroupError from './../../src/common/components/forms/HorizontalFormGroupError';
+
+describe('HorizontalFormGroupError', () => {
+    const defaultProps = {
+        id: 'form',
+        label: 'label'        
+    }
+
+    it('renders ok', () => {
+        const wrapper = shallow(<HorizontalFormGroupError {...defaultProps} />);
+        expect(wrapper).to.be.ok;        
+    })
+
+    it('should displays error when any', () => {
+        const wrapper = shallow(<HorizontalFormGroupError {...defaultProps} error="any error displayed here" />);
+        expect(wrapper.contains('any error displayed here')).to.be.true;
+    })
+
+    it('should renders children when any', () => {
+        const wrapper = shallow(
+            <HorizontalFormGroupError {...defaultProps}>
+                <p>Child rendered</p>
+            </HorizontalFormGroupError>
+        );
+
+        expect(wrapper.contains(<p>Child rendered</p>)).to.be.true;
+    })
+})
--- test/transactions/CategoryForm.spec.js
+++ test/transactions/CategoryForm.spec.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
 import sinon from 'sinon';
-import moxios from 'moxios'
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
-import { Provider } from 'react-redux';
-import moment from 'moment';
 
 import CategoryForm from './../../src/transactions/components/CategoryForm';
 import { 
