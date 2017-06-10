f5d51d05ae95b931f5b8fb048e57acbc4314fd08
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-06-10 11:10:56 -0300

    test heroku

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import { isAuthenticated, getToken } from './session';
 export const BASE_URL = process.env.API_BASE_URL;//test heroku || 'http://localhost:8000/api/v1/';
 
 export default function createAPI() {
-    console.log('HEROKU BASE_URL:' + process.env.API_BASE_URL);
+    console.log('FROM HEROKU:' + BASE_URL);
     let auth = { }
     if (isAuthenticated()) {
         auth = {
