665641e60ae63b18c9619e95ac20f497d81a742d
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-07-06 22:05:22 -0300

    QA on generic error handler

--- src/services/genericErrorHandler.js
+++ src/services/genericErrorHandler.js
@@ -4,9 +4,8 @@ export default function tryHandleAPIResponse(err) {
     if (err.response) {
         return err.response.data;
     }
-    else {        
-        console.error('Unhandled error: ' + err.message);
-        console.error(err);
+    else {
+        console.error('Unhandled error: ' + err.message, err);        
         return { [UNKNOWN_ERROR_NAME]: err.message };
     }
 }
