99a849c5330234c3ef16c99ac3419044eeaef391
guilatrova <guilhermelatrova@hotmail.com>
2017-07-20 13:22:11 -0300

    Updated prop types references

--- src/transactions/transactions/components/TransactionForm.js
+++ src/transactions/transactions/components/TransactionForm.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import DatetimeInput from 'react-datetime';
 import CurrencyInput from 'react-currency-input';
