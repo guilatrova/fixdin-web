ba4dfb7b5ebcfb471c75ce3a601a38c145e52b98
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-08-14 05:17:23 -0300

    Removed unused tests from TransactionTable

--- test/transactions/TransactionTable.spec.js
+++ test/transactions/TransactionTable.spec.js
@@ -21,9 +21,9 @@ describe('TransactionTable', () => {
     }
 
     const transactions = [
-            { id: 1, value: '10', due_date: moment(new Date()) },
-            { id: 2, value: '-11', due_date: moment(new Date()) },
-            { id: 3, value: '12', due_date: moment(new Date()) },
+            { id: 1, value: 10, due_date: moment(new Date()) },
+            { id: 2, value: -11, due_date: moment(new Date()) },
+            { id: 3, value: 12, due_date: moment(new Date()) },
         ]
 
     it('renders ok', () => {
@@ -31,6 +31,11 @@ describe('TransactionTable', () => {
         expect(wrapper).to.be.ok;
     });
 
+    it('should render transactions in table', () => {
+        const wrapper = mount(<TransactionTable {...defaultProps} transactions={transactions} />);
+        expect(wrapper.find('tr').length).to.equal(4); //1 TH + 3 TRs
+    });
+    
     describe('sorts', () => {
 
         const wrapper = shallow(<TransactionTable {...defaultProps} />);
@@ -139,41 +144,4 @@ describe('TransactionTable', () => {
 
     });
 
-    xit('should render transactions in table', () => {
-        const wrapper = mount(<TransactionTable {...defaultProps} transactions={transactions} />);
-        expect(wrapper.find('tr').length).to.equal(4); //1 TH + 3 TRs
-    })
-
-    xit('should calls onEdit', () => {
-        let editSpy = sinon.spy();
-
-        const wrapper = mount(<TransactionTable {...defaultProps} transactions={transactions} onEdit={editSpy} />);
-
-        wrapper.find('.edit-button').first().simulate('click');
-
-        expect(editSpy.called).to.be.true;
-        expect(editSpy.alwaysCalledWith(1)).to.be.true;
-    })
-
-    xit('should calls onDelete', () => {
-        let deleteSpy = sinon.spy();
-
-        const wrapper = mount(<TransactionTable {...defaultProps} transactions={transactions} onDelete={deleteSpy} />);
-
-        wrapper.find('.delete-button').first().simulate('click');
-
-        expect(deleteSpy.called).to.be.true;
-        expect(deleteSpy.alwaysCalledWith(1)).to.be.true;
-    })
-
-    xit('should calls onCopy', () => {
-        let copySpy = sinon.spy();
-
-        const wrapper = mount(<TransactionTable {...defaultProps} transactions={transactions} onCopy={copySpy} />);
-
-        wrapper.find('.copy-button').first().simulate('click');
-
-        expect(copySpy.called).to.be.true;
-        expect(copySpy.alwaysCalledWith(1)).to.be.true;
-    })    
 })
