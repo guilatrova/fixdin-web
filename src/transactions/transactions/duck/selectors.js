078426bb478d32c6977837600beaf7720172b1ae
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-10-03 07:47:06 -0300

    Added selectors tests

--- src/transactions/transactions/duck/selectors.js
+++ src/transactions/transactions/duck/selectors.js
@@ -1,5 +1,13 @@
 const getErrors = (state) => state.transactions.errors;
 
+const getTransactionsToDisplay = (state) => {
+    if (state.transactions.visibleTransactions.length > 0) {
+        return state.transactions.visibleTransactions;
+    }
+    return state.transactions.transactions
+};
+
 export default {
-    getErrors
+    getErrors,
+    getTransactionsToDisplay
 };
--- src/transactions/transactions/duck/selectors.spec.js
+++ src/transactions/transactions/duck/selectors.spec.js
@@ -1,4 +1,60 @@
 import selectors from './selectors';
 
-xdescribe('balances/transactions/selectors', () => {
+describe('transactions/duck/selectors', () => {
+    const buildState = (state) => {
+        return {
+            transactions: state
+        }
+    }
+
+    const buildTransactionsUntil = (length) => {
+        const transactions = []
+        for (let i = 0; i < length; i++) {
+            transactions[i] = { id: i+1 };
+        }
+        return transactions
+    }
+
+    it('getErrors', () => {
+        const state = buildState({
+            errors: 'errors'
+        });
+
+        expect(
+            selectors.getErrors(state)
+        )
+        .to.deep.equal('errors');
+    });
+
+    describe('getTransactionsToDisplay', () => {
+        
+        it('should return visibleTransactions', () => {        
+            const visibleTransactions = buildTransactionsUntil(3);
+            const transactions = buildTransactionsUntil(5);
+            const state = buildState({
+                transactions,
+                visibleTransactions
+            });
+
+            expect(
+                selectors.getTransactionsToDisplay(state)
+            )
+            .to.deep.equal(visibleTransactions);
+        })
+
+        it('should return all when visibleTransactions is empty', () => {
+            const transactions = buildTransactionsUntil(2);
+            const state = buildState({
+                transactions,
+                visibleTransactions: []
+            });
+
+            expect(
+                selectors.getTransactionsToDisplay(state)
+            )
+            .to.deep.equal(transactions);
+        });        
+
+    })
+    
 });
