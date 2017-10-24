cfd5acef9f75915b04ba72bd5cfec6c4244682c0
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-10-24 06:27:03 -0200

    redirects to login when token has expired

--- src/services/genericErrorHandler.js
+++ src/services/genericErrorHandler.js
@@ -1,11 +1,25 @@
 const UNKNOWN_ERROR_NAME = 'detail';
+const TOKEN_EXPIRED_MESSAGE = 'Token has expired'
+
+const isTokenExpiredError = (err) => {
+    const { response } = err;
+    return (response.status === 401 && response.data['detail'] === TOKEN_EXPIRED_MESSAGE);
+}
+
+const redirectToLogin = () => {
+    const baseUrl = window.location.origin;    
+    window.location.replace(`${baseUrl}/Login`);
+}
 
 export default function tryHandleAPIResponse(err) {
     if (err.response) {
+        if (isTokenExpiredError(err)) {
+            redirectToLogin();
+        }
         return err.response.data;
     }
     else {
-        console.error('Unhandled error: ' + err.message, err);        
+        console.error('Unknown error: ' + err.message, err);
         return { [UNKNOWN_ERROR_NAME]: err.message };
     }
 }
