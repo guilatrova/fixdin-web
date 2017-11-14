23e0caf6d55b4a4da2b8507cf8e6f05b86d5f1e2
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-11-14 14:31:30 -0200

    Added genericErrorHandler and improved expect to work with jest

--- tools/testFrameworkSetup.js
+++ tools/testFrameworkSetup.js
@@ -1,4 +1,4 @@
-import { expect as chaiExpect } from 'chai';
+import chai from 'chai';
 
 class LocalStorageMock {
     constructor() {
@@ -23,4 +23,25 @@ class LocalStorageMock {
 }
   
 global.localStorage = new LocalStorageMock;
-global.expect = chaiExpect;
+
+const originalNot = Object.getOwnPropertyDescriptor(chai.Assertion.prototype, 'not').get;
+Object.defineProperty(chai.Assertion.prototype, 'not', {
+    get() {
+        Object.assign(this, this.assignedNot);
+        return originalNot.apply(this);
+    },
+    set(newNot) {
+        this.assignedNot = newNot;
+        return newNot;
+    }
+});
+
+// Combine both jest and chai matchers on expect
+const jestExpect = global.expect;
+
+global.expect = actual => {
+    const originalMatchers = jestExpect(actual);
+    const chaiMatchers = chai.expect(actual);
+    const combinedMatchers = Object.assign(chaiMatchers, originalMatchers);
+    return combinedMatchers;
+};
