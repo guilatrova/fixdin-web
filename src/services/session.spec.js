1f3814070d341ea1657ad9f4fcb32f96c231a20f
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-11-14 13:57:20 -0200

    Added session tests

---
+++ src/services/session.spec.js
@@ -0,0 +1,28 @@
+import * as sessionModule from './session';
+
+describe('services/session', () => {
+
+    const SAVED_TOKEN = 'abc123456';
+
+    beforeEach(() => {
+        localStorage.clear();
+    });
+
+    it('should save token', () => {
+        sessionModule.saveToken(SAVED_TOKEN);
+        
+        expect(localStorage.getItem(sessionModule.TOKEN_KEY)).to.equal(SAVED_TOKEN);
+    });
+
+    it('should retrieve token', () => {
+        localStorage.setItem(sessionModule.TOKEN_KEY, SAVED_TOKEN);
+
+        expect(sessionModule.getToken()).to.equal(SAVED_TOKEN);
+    });
+
+    it('authentication should return True when has token', () => {
+        localStorage.setItem(sessionModule.TOKEN_KEY, SAVED_TOKEN);
+        
+        expect(sessionModule.isAuthenticated()).to.be.true;
+    });
+});
