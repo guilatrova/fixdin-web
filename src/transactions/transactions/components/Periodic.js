f2c48bd2907fad76a82bc8d24cf433df27f265d7
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-11-17 10:03:34 -0200

    Added Periodic component

---
+++ src/transactions/transactions/components/Periodic.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { DatePicker } from 'material-ui-pickers';
+import TextFieldError from './../../../common/material/TextFieldError';
+
+import { FormControl } from 'material-ui/Form';
+import Input, { InputLabel } from 'material-ui/Input';
+import Select from 'material-ui/Select';
+import { MenuItem } from 'material-ui/Menu';
+
+export default class Periodic extends React.Component{    
+    static propTypes = {
+        visible: PropTypes.bool,
+        onChange: PropTypes.func
+    }
+
+    state = {
+        frequency: 'daily',
+        interval: "",
+        how_many: "",
+        until: undefined
+    }
+
+    handleChange(key, value) {
+        this.setState({ [key]: value }, () => {
+            this.props.onChange(this.state);
+        });
+    }
+
+    handleLimitChange(key, value) {
+        if (key === 'until') {
+            this.setState({ how_many: "" });
+        }
+        else {
+            this.setState({ until: undefined });
+        }
+        this.handleChange(key, value);
+    }
+
+    render() {
+        const { visible } = this.props;
+
+        if (!visible) 
+            return null;
+
+        return (
+            <div>
+            <TextFieldError
+                id="interval"
+                label="A cada"
+                value={this.state.interval}
+                onChange={e => this.handleChange('interval', e.target.value)} />
+            
+            <FormControl>
+                <InputLabel htmlFor="input-frequency">Frequência</InputLabel>
+                <Select
+                    name="frequency"
+                    value={this.state.frequency}
+                    onChange={e => this.handleChange('frequency', e.target.value)}
+                    input={<Input id="input-frequency" />}
+                >
+                    <MenuItem value={'daily'}>dias</MenuItem>
+                    <MenuItem value={'weekly'}>semanas</MenuItem>
+                    <MenuItem value={'monthly'}>meses</MenuItem>
+                    <MenuItem value={'yearly'}>anos</MenuItem>
+                </Select>
+            </FormControl>
+
+            <TextFieldError
+                id="how_many"
+                label="vezes"                        
+                value={this.state.how_many}
+                onChange={e => this.handleLimitChange('how_many', e.target.value)} />
+
+            <FormControl>
+                <InputLabel htmlFor="until">Até</InputLabel>
+                <DatePicker
+                    value={this.state.until}
+                    onChange={value => this.handleLimitChange('until', value)}
+                    autoOk={true}
+                    format="DD MMMM YYYY"
+                />
+            </FormControl>
+            </div>
+        );
+    }
+}
