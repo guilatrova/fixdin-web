e1bea815cd8d6cd39b8e6b9c0a014ab9159727ae
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-11-17 10:27:03 -0200

    Added TransactionTable with tests

---
+++ src/transactions/transactions/components/TransactionTable.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+import { MenuItem } from 'material-ui/Menu';
+
+import DeleteIcon from 'material-ui-icons/Delete';
+import ContentCopyIcon from 'material-ui-icons/ContentCopy';
+import EditIcon from 'material-ui-icons/ModeEdit';
+import MoneyIcon from 'material-ui-icons/AttachMoney';
+
+// import { 
+//     DescriptionFilter, 
+//     CategoryFilter, 
+//     DueDateFilter, 
+//     KindFilter, 
+//     DeadlineFilter,
+//     PriorityFilter 
+// } from './filters';
+import { sort } from './../../../utils/sorts';
+import TableSort from './../../../common/material/TableSort';
+import DataColumn from './../../../common/material/DataColumn';
+import CollapsibleMenu from './../../../common/material/CollapsibleMenu';
+import { getKind } from '../../kinds';
+
+class TransactionTable extends React.Component {
+    static propTypes = {
+        classes: PropTypes.object.isRequired,
+        transactions: PropTypes.array.isRequired,
+        categories: PropTypes.array.isRequired,
+        onEdit: PropTypes.func.isRequired,
+        onPay: PropTypes.func.isRequired,
+        onCopy: PropTypes.func.isRequired,
+        onDelete: PropTypes.func.isRequired,
+        isFetching: PropTypes.bool.isRequired
+    }
+    
+    constructor(props) {
+        super(props);
+
+        this.formatCategory = this.formatCategory.bind(this);
+        this.formatOptions = this.formatOptions.bind(this);
+        this.sortCategory = this.sortCategory.bind(this);
+    }
+
+    //Format
+    formatDate(row, field) {
+        const cell = row[field];
+        return cell ? cell.format('DD/MM/YYYY') : 'NÃO PAGO';
+    }
+
+    formatCategory(row, field) {
+        const cell = row[field];
+        const { categories } = this.props;
+        return categories.length === 0 ? '' : categories.find((category) => category.id == cell).name;
+    }
+
+    formatValue(row, field) {
+        const cell = row[field];
+        let positiveValue = cell.toFixed(2).toString().replace(".", ",");
+        if (positiveValue[0] == '-') {
+            positiveValue = positiveValue.substring(1);
+        }
+        return `R$ ${positiveValue}`;
+    }
+
+    formatOptions(transaction) {
+        const { onEdit, onDelete, onCopy, onPay, isFetching } = this.props;
+        const pendingPayment = !transaction.payment_date;
+
+        return (
+            <div>
+                {pendingPayment && <div className="col-xs-6">
+                    <MenuItem onClick={() => onPay(transaction.id)} disabled={isFetching}><MoneyIcon /></MenuItem>
+                </div>}
+                <div className="col-xs-6 pull-right">
+                    <CollapsibleMenu>
+                        <MenuItem onClick={() => onEdit(transaction.id)}><EditIcon /> Editar</MenuItem>
+                        <MenuItem onClick={() => onCopy(transaction.id)}><ContentCopyIcon /> Copiar</MenuItem>
+                        <MenuItem onClick={() => onDelete(transaction.id)}><DeleteIcon /> Deletar</MenuItem>
+                    </CollapsibleMenu>
+                </div>
+            </div>
+        );
+    }
+
+    formatKind(transaction) {
+        return getKind(transaction.kind).text;
+    }
+
+    //Sort
+    sortValue(a, b, order) {
+        let valueA = Number(a);
+        let valueB = Number(b);
+
+        if (valueA < 0)
+            valueA = -valueA;
+
+        if (valueB < 0)
+            valueB = -valueB;
+
+        if (order === 'asc')
+            return valueA - valueB;
+
+        return valueB - valueA;
+    }
+
+    sortCategory(a, b, order) {
+        const { categories } = this.props;
+        const catA = categories.find((category) => category.id == a).name.toUpperCase();
+        const catB = categories.find((category) => category.id == b).name.toUpperCase();
+
+        return sort(catA, catB, order);
+    }
+
+    sortDate(a, b, order) {//TODO: Move it to utils, its too generic
+        if (moment.isMoment(a) && moment.isMoment(b)) { 
+            if (order === 'desc') 
+                return b.unix() - a.unix(); 
+            else 
+                return a.unix() - b.unix(); 
+        } 
+ 
+        if (moment.isMoment(a)) { 
+            return (order === 'desc') ? 1 : -1; 
+        } 
+    
+        if (moment.isMoment(b)) { 
+            return (order === 'desc') ? -1 : 1; 
+        } 
+    
+        return 0; 
+    }
+
+    render () {
+        return (
+            <TableSort data={this.props.transactions} initialOrderBy="due_date">
+
+                <DataColumn sortable field="kind" onRender={this.formatKind}>Tipo</DataColumn>
+                <DataColumn sortable field="due_date" onRender={this.formatDate} onSort={this.sortDate}>Vencimento</DataColumn>
+                <DataColumn sortable field="description">Descrição</DataColumn>
+
+                <DataColumn 
+                    sortable
+                    field="category" 
+                    onRender={this.formatCategory}
+                    onSort={this.sortCategory}>
+                    
+                    Categoria
+                </DataColumn>
+
+                <DataColumn
+                    sortable 
+                    field="value" 
+                    onRender={this.formatValue}
+                    onSort={this.sortValue}>
+                    
+                    Valor
+                </DataColumn>
+
+                <DataColumn sortable numeric field="priority">Importancia</DataColumn>
+                <DataColumn sortable numeric field="deadline">Tolerância</DataColumn>
+                <DataColumn sortable field="payment_date" onRender={this.formatDate} onSort={this.sortDate}>Pago em</DataColumn>
+                <DataColumn onRender={this.formatOptions} />
+            </TableSort>
+        );
+    }
+}
+
+export default TransactionTable;
---
+++ src/transactions/transactions/components/TransactionTable.spec.js
@@ -0,0 +1,136 @@
+import TransactionTable from './TransactionTable';
+import moment from 'moment';
+
+describe('<TransactionTable />', () => {
+
+    const defaultProps = {
+        onRefresh: () => {},
+        onEdit: () => {},
+        onCopy: () => {},
+        isFetching: false,
+        kind: 'income',
+        transactions: [],
+        categories: [
+            { undefined: 'any' }
+        ]
+    };
+
+    it('renders ok', () => {
+        const wrapper = shallow(<TransactionTable {...defaultProps} />);
+        expect(wrapper).to.be.ok;
+    });
+
+    xit('should render transactions in table', () => {
+        // const wrapper = mount(<TransactionTable {...defaultProps} transactions={transactions} />);
+        // expect(wrapper.find('tr').length).to.equal(4); //1 TH + 3 TRs
+    });
+    
+    describe('sorts', () => {
+
+        const wrapper = shallow(<TransactionTable {...defaultProps} />);
+        const instance = wrapper.instance();
+
+        describe('value', () => {
+
+            it('asc', () => {
+                expect(instance.sortValue("150", "200", 'asc')).to.be.lessThan(0);
+                expect(instance.sortValue("200", "150", 'asc')).to.be.greaterThan(0);
+            });
+
+            it('desc', () => {
+                expect(instance.sortValue("150", "200", 'desc')).to.be.greaterThan(0);
+                expect(instance.sortValue("200", "150", 'desc')).to.be.lessThan(0);
+            });
+
+            it('with decimals', () => {
+                expect(instance.sortValue("150.15", "150.16", 'asc')).to.be.lessThan(0);
+                expect(instance.sortValue("150.16", "150.15", 'asc')).to.be.greaterThan(0);
+            });
+
+            it('regardless of signal', () => {
+                expect(instance.sortValue("150", "-200", "asc")).to.be.lessThan(0);
+            });
+
+        });
+
+        describe('category', () => {
+
+            const categories = [
+                { id: 1, name: 'A' },
+                { id: 2, name: 'a' },
+                { id: 3, name: 'B' },
+                { id: 4, name: 'b' },
+            ];
+            const wrapper = shallow(<TransactionTable {...defaultProps} categories={categories} />);
+            const instance = wrapper.instance();
+
+            it('asc', () => {
+                expect(instance.sortCategory(1, 3, 'asc')).to.be.lessThan(0);
+                expect(instance.sortCategory(3, 1, 'asc')).to.be.greaterThan(0);
+            });
+
+            it('desc', () => {
+                expect(instance.sortCategory(1, 3, 'desc')).to.be.greaterThan(0);
+                expect(instance.sortCategory(3, 1, 'desc')).to.be.lessThan(0);
+            });
+
+            it('regardless of casing', () => {
+                expect(instance.sortCategory(2, 3, 'asc')).to.be.lessThan(0);
+                expect(instance.sortCategory(3, 2, 'asc')).to.be.greaterThan(0);
+            });            
+        });
+
+        describe('date', () => {
+
+            const firstJanuary = moment('2017-01-01', 'YYYY-MM-DD');
+            const secondJanuary = moment('2017-01-02', 'YYYY-MM-DD');
+
+            it('asc', () => {
+                expect(instance.sortDate(firstJanuary, secondJanuary, 'asc')).to.be.lessThan(0);
+                expect(instance.sortDate(secondJanuary, firstJanuary, 'asc')).to.be.greaterThan(0);
+            });
+
+            it('desc', () => {
+                expect(instance.sortDate(secondJanuary, firstJanuary, 'desc')).to.be.lessThan(0);
+                expect(instance.sortDate(firstJanuary, secondJanuary, 'desc')).to.be.greaterThan(0);
+            });
+
+            it('with invalid date', () => {
+                expect(instance.sortDate(firstJanuary, undefined, 'asc')).to.be.lessThan(0);
+                expect(instance.sortDate(undefined, firstJanuary, 'asc')).to.be.greaterThan(0);
+                expect(instance.sortDate(firstJanuary, undefined, 'desc')).to.be.greaterThan(0);
+                expect(instance.sortDate(undefined, firstJanuary, 'desc')).to.be.lessThan(0);
+            });
+        });
+
+    });
+
+    describe('formats', () => {
+        const categories = [ { id: 1, name:'a' } ];
+        const wrapper = shallow(<TransactionTable {...defaultProps} categories={categories} />);
+        const instance = wrapper.instance();
+        const row = {
+            category: 1,
+            value: -10,
+            date: moment('2014-08-22', 'YYYY-MM-DD')
+        };
+
+        it('value', () => {
+            expect(instance.formatValue(row, 'value')).to.be.equal('R$ 10,00');
+        });
+
+        it('date', () => {
+            expect(instance.formatDate(row, 'date')).to.be.equal('22/08/2014');
+        });
+
+        it('undefined date', () => {
+            expect(instance.formatDate(row, 'undefined')).to.be.equal('NÃO PAGO');
+        });
+
+        it('category', () => {
+            expect(instance.formatCategory(row, 'category')).to.be.equal('a');
+        });
+
+    });
+
+});
