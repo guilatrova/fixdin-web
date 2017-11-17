fd6cb09fd9bc0141f186475edd46cbfdc13a1a7a
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-11-17 07:00:17 -0200

    Moved App component

---
+++ src/app/containers/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+import AppBodyContainer from './AppBodyContainer';
+
+import CategoryPage from  '../../transactions/categories/containers/CategoryPage';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<AppBodyContainer>
+					<Switch>
+						<Route exact path="/" component={CategoryPage} />
+						{/* <Route exact path="/home" component={HomePage} /> */}
+						{/* <Route exact path="/test" com1ponent={() => "TESTE"} /> */}
+						{/* <Route path="/fuel-savings" component={FuelSavingsPage} /> */}
+						{<Route component={() => "not found"} />}
+					</Switch>
+				</AppBodyContainer>
+			</div>
+		);
+	}
+}
+
+App.propTypes = {
+	children: PropTypes.element
+};
+
+export default App;
