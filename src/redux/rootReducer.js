c279ddc83bf46c4e7e4e63e7a6d16947b0fdda20
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-11-17 06:39:49 -0200

    Added some root reducers

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,18 @@
 import { combineReducers } from 'redux';
+//import users from '../users/duck';
+import transactions from '../transactions/transactions/duck';
+import categories from '../transactions/categories/duck';
+import balances from '../balances/duck';
+//import reports from '../reports/duck';
+import paymentOrders from '../paymentOrders/duck';
+import integrations from '../integrations/CPFL/duck';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({  
+    transactions,
+    balances,
+    categories,
+    paymentOrders,
+    integrations
+});
 
 export default rootReducer;
