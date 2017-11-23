b7c376eddc330f433a422794f39859c2a09b63a7
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-11-23 06:38:13 -0200

    Added TransactionList

---
+++ src/transactions/transactions/components/TransactionList.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { withStyles } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+import List, { ListItem, ListItemSecondaryAction, ListItemText } from 'material-ui/List';
+import Checkbox from 'material-ui/Checkbox';
+
+import { formatCurrencyDisplay } from '../../../services/formatter';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        background: theme.palette.background.paper,
+    },
+});
+
+class TransactionList extends React.Component {
+    static propTypes = {
+        transactions: PropTypes.array.isRequired
+    }
+
+    state = {
+        checked: []
+    };
+
+    handleToggle(value) {
+        const { checked } = this.state;
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+    
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+    
+        this.setState({
+            checked: newChecked,
+        });
+    }
+
+    render() {
+
+        const transactionItems = this.props.transactions.map(transaction => {
+            const { id, description } = transaction;
+            const value = formatCurrencyDisplay(transaction.value);
+
+            return (
+                <ListItem dense button key={id}>
+                    <Checkbox
+                        onClick={() => this.handleToggle(id)}
+                        checked={this.state.checked.indexOf(id) !== -1} />
+                    <ListItemText primary={description} />
+
+                    <ListItemSecondaryAction>
+                        <ListItemText primary={value} />
+                    </ListItemSecondaryAction>
+
+                </ListItem>
+            );
+        });
+
+        return (
+            <div style={{maxHeight: 300, overflow: 'auto'}}>
+                {transactionItems.length == 0 && <Typography type="body2">Não há nada aqui</Typography>}
+                <List>
+                    {transactionItems}
+                </List>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(TransactionList);
---
+++ src/transactions/transactions/components/TransactionList.spec.js
@@ -0,0 +1,9 @@
+import TransactionList from './TransactionList';
+
+describe('<TransactionList />', () => {
+
+    it('renders ok', () => {
+        expect(<TransactionList />).to.be.ok;
+    });
+
+});
