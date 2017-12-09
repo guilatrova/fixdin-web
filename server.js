aeef40d8f8015bcf50a2ed83709abf2601c5244e
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-12-09 09:32:54 -0200

    Fixed routing with server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const express = require('express');
-const app = express();
+/* eslint-disable */
+var express = require('express');
+var path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server started port: ${port}`);
+  }
+});
