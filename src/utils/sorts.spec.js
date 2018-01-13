cd8eff72dd8db596a823e7536ee8864c24660df2
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-01-13 09:40:47 -0200

    Updated sorts.spec to use jest

--- src/utils/sorts.spec.js
+++ src/utils/sorts.spec.js
@@ -5,18 +5,18 @@ describe('sorts', () => {
     describe('alpha', () => {
 
         it('asc', () => {
-            expect(sort('a', 'b', 'asc')).to.be.lessThan(0);
-            expect(sort('b', 'a', 'asc')).to.be.greaterThan(0);
+            expect(sort('a', 'b', 'asc')).toBeLessThan(0);
+            expect(sort('b', 'a', 'asc')).toBeGreaterThan(0);
         });
 
         it('desc', () => {
-            expect(sort('b', 'a', 'desc')).to.be.lessThan(0);
-            expect(sort('a', 'b', 'desc')).to.be.greaterThan(0);
+            expect(sort('b', 'a', 'desc')).toBeLessThan(0);
+            expect(sort('a', 'b', 'desc')).toBeGreaterThan(0);
         });
 
         it('regardless of casing', () => {
-            expect(sort('a', 'B', 'asc')).to.be.lessThan(0);
-            expect(sort('A', 'b', 'asc')).to.be.lessThan(0);
+            expect(sort('a', 'B', 'asc')).toBeLessThan(0);
+            expect(sort('A', 'b', 'asc')).toBeLessThan(0);
         });
 
     });
@@ -24,17 +24,17 @@ describe('sorts', () => {
     describe('numbers', () => {
 
         it('asc', () => {
-            expect(sort(1, 2, 'asc')).to.be.lessThan(0);
-            expect(sort(2, 1, 'asc')).to.be.greaterThan(0);
+            expect(sort(1, 2, 'asc')).toBeLessThan(0);
+            expect(sort(2, 1, 'asc')).toBeGreaterThan(0);
         });
 
         it('desc', () => {
-            expect(sort(2, 1, 'desc')).to.be.lessThan(0);
-            expect(sort(1, 2, 'desc')).to.be.greaterThan(0);
+            expect(sort(2, 1, 'desc')).toBeLessThan(0);
+            expect(sort(1, 2, 'desc')).toBeGreaterThan(0);
         });
 
         it('regardless of its type', () => {
-            expect(sort('10', 9, 'asc')).to.be.greaterThan(0);
+            expect(sort('10', 9, 'asc')).toBeGreaterThan(0);
         });
 
     });
@@ -42,13 +42,13 @@ describe('sorts', () => {
     describe('alphanumerics', () => {
         
         it('asc', () => {
-            expect(sort('a', 1, 'asc')).to.be.greaterThan(0);
-            expect(sort(1, 'a', 'asc')).to.be.lessThan(0);
+            expect(sort('a', 1, 'asc')).toBeGreaterThan(0);
+            expect(sort(1, 'a', 'asc')).toBeLessThan(0);
         });
 
         it('desc', () => {
-            expect(sort('a', 1, 'desc')).to.be.lessThan(0);
-            expect(sort(1, 'a', 'desc')).to.be.greaterThan(0);
+            expect(sort('a', 1, 'desc')).toBeLessThan(0);
+            expect(sort(1, 'a', 'desc')).toBeGreaterThan(0);
         });
 
     });
