0381f0c1a64162b1a2d9cc09e973c579840ba538
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-07-29 15:04:09 -0300

    Added title top bar and style to selected tab

--- src/transactions/transactions/components/TransactionTableBar.js
+++ src/transactions/transactions/components/TransactionTableBar.js
@@ -7,16 +7,24 @@ import ClearAllIcon from '@material-ui/icons/ClearAll';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import AppBar from '@material-ui/core/AppBar';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import { Tabs, Tab } from '../../../common/material/Tabs';
 
 const styles = theme => ({
     bar: {
-        position: "relative"
+        position: "relative",
+        padding: "10px 0 0 15px",
+        boxSizing: "border-box",
     },
     button: {
         margin: "0 5px",
         ...theme.mixins.fab
+    },
+    tabButton: {
+        [theme.breakpoints.up('md')]: {
+            minWidth: 90,
+        },
     },
     container: {
         position: "absolute",
@@ -42,12 +50,18 @@ const TransactionTableBar = ({ tab, classes, onTabChange, onAdd, onRefresh, onCl
         );
     };
 
+    const tabClasses = { classes: { root: classes.tabButton, wrapper: classes.tabButton } };
+
     return (
         <AppBar position="static" className={classes.bar}>
+            <Typography variant="subheading" color="secondary">
+                OPERAÇÕES FINANCEIRAS REALIZADAS
+            </Typography>
+
             <Tabs value={tab} onChange={onTabChange}>
-                <Tab label="Todas" />
-                <Tab label="Receitas" />
-                <Tab label="Despesas" />
+                <Tab label="Todas" {...tabClasses} />
+                <Tab label="Receitas" {...tabClasses} />
+                <Tab label="Despesas" {...tabClasses} />
             </Tabs>
 
             <div className={classes.container}>
