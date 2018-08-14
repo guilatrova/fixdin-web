08ae5845d5a6033c33ee32b85e7f60ea6d6642b7
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-08-14 20:34:31 -0300

    removed console log commands

--- src/paymentOrders/specifications.js
+++ src/paymentOrders/specifications.js
@@ -1,19 +1,13 @@
 import transactionSpecifications from '../transactions/transactions/specifications';
 
-const hasAnyTransaction = (group, periodKey) => {
-    console.log(`${periodKey} has ${group[periodKey].length} items`);
-    return group[periodKey].length > 0;
-};
+const hasAnyTransaction = (group, periodKey) => group[periodKey].length > 0;
 
 const isEverythingToDate = (transactionGroup) =>
     Object.keys(transactionGroup)
         .filter(periodKey => hasAnyTransaction(transactionGroup, periodKey))
         .every(periodKey => transactionGroup[periodKey].every(
-            t => {
-                console.log("here");
-                return !transactionSpecifications.isOverdue(t);
-            }
-        ));
+            t => !transactionSpecifications.isOverdue(t))
+        );
 
 const isAnythingPending = (transactionGroup) =>
     Object.keys(transactionGroup)
