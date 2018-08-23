50ee1793d352901062a807b1272f623366b40c93
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-08-23 06:25:35 -0300

    Moved arrow style to new scss

--- src/common/material/Tabs/Tab.js
+++ src/common/material/Tabs/Tab.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import cn from 'classnames';
 
 import Tab from '@material-ui/core/Tab';
 
 const CustomTab = ({ classes, ...remainingProps }) => {
     return (
         <Tab
-            classes={{ selected: "tab-selected", ...classes }}
+            classes={{ selected: cn("tab-selected", "arrow-parent"), ...classes }}
             {...remainingProps}
         />
     );
