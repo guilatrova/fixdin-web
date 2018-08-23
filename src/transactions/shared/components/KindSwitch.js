aa69aad22ea256685928f5e3b13d73f85053b988
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-08-23 07:06:44 -0300

    Improve KindSwitch style

--- src/transactions/shared/components/KindSwitch.js
+++ src/transactions/shared/components/KindSwitch.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
+import FormLabel from '@material-ui/core/FormLabel';
 
 import { EXPENSE, INCOME } from '../kinds';
 
-const KindSwitch = ({ value, onChange }) => {
+const styles = {
+    root: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    selected: {
+        fontWeight: 'bold'
+    }
+};
+
+const KindSwitch = ({ classes, value, onChange }) => {
     let expenseProps, incomeProps;
-    
+
     if (value.id == EXPENSE.id) {
-        expenseProps = { color: 'primary' };
-        incomeProps = { };
+        expenseProps = { color: 'primary', variant: 'outlined', className: classes.selected };
+        incomeProps = {};
     }
     else {
-        expenseProps = { };
-        incomeProps = { color: 'primary' };
+        expenseProps = {};
+        incomeProps = { color: 'primary', variant: 'outlined', className: classes.selected };
     }
 
     return (
-        <div>
-            <Button {...incomeProps} onClick={() => onChange(INCOME)}>Receita</Button>
-            <Button {...expenseProps} onClick={() => onChange(EXPENSE)}>Despesa</Button>
+        <div className={classes.root}>
+            <FormLabel component="legend">Tipo</FormLabel>
+
+            <div>
+                <Button {...incomeProps} onClick={() => onChange(INCOME)}>Receita</Button>
+                <Button {...expenseProps} onClick={() => onChange(EXPENSE)}>Despesa</Button>
+            </div>
         </div>
-    );    
+    );
 };
 
 KindSwitch.propTypes = {
@@ -34,4 +51,4 @@ KindSwitch.defaultProps = {
     value: EXPENSE
 };
 
-export default KindSwitch;
+export default withStyles(styles)(KindSwitch);
