eb0b33c78e3e3d2f00c0f0836fc16f7ab84b1af6
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-10-09 07:58:52 -0300

    Added dynamic changes

--- src/charts/TransactionsDonutChart.js
+++ src/charts/TransactionsDonutChart.js
@@ -11,71 +11,87 @@ const expenseColor = blue["300"];
 const incomeColor = purple['500'];
 const duration = 1500;
 
-const TransactionsDonutChart = ({ incomes, expenses, total }) => {
+class TransactionsDonutChart extends React.Component {
+    static propTypes = {
+        data: PropTypes.object,
+        incomes: PropTypes.number,
+        expenses: PropTypes.number,
+        total: PropTypes.number
+    }
 
-    const totalDisplay = formatCurrencyDisplay(total);
+    componentWillReceiveProps(nextProps) {
+        const { incomes, expenses, total } = nextProps;
+        const chart = this.refs.chart.getChart();
 
-    const config = {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie',
-            height: 220,
-            animation: {
-                duration: duration
-            }
-        },
-        credits: false,
-        title: {
-            text: totalDisplay,
-            align: 'center',
-            verticalAlign: 'middle',
-            y: 10,
-            style: {
-                color: total < 0 ? expenseColor : 'inherit'
-            }
-        },
-        tooltip: {
-            pointFormat: '<b>{point.percentage:.2f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                startAngle: -35,
-                colors: [expenseColor, incomeColor],
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b><br/> R$ {point.y:.2f}'
-                }
-            }
-        },
-        series: [{
-            name: 'Transações',
-            innerSize: '80%',
-            data: [{
+        const data = this.getData(incomes, expenses);
+        const totalDisplay = formatCurrencyDisplay(total);
+
+        chart.series[0].update({ data });
+        chart.title.update({ text: totalDisplay });
+    }
+
+    getData = (incomes, expenses) => (
+        [
+            {
                 name: 'Despesas',
-                y: -expenses
+                y: incomes
             }, {
                 name: 'Receitas',
-                y: incomes
-            }]
-        }]
-    };
-
-    return (
-        <div>
-            <ReactHighcharts config={config} />
-        </div>
+                y: -expenses
+            }
+        ]
     );
-};
 
-TransactionsDonutChart.propTypes = {
-    data: PropTypes.object,
-    incomes: PropTypes.number,
-    expenses: PropTypes.number,
-    total: PropTypes.number
-};
+    render() {
+        const config = {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie',
+                height: 220,
+                animation: {
+                    duration: duration
+                }
+            },
+            credits: false,
+            title: {
+                text: "R$ 0,00",
+                align: 'center',
+                verticalAlign: 'middle',
+                y: 10,
+                // style: {
+                //     color: total < 0 ? expenseColor : 'inherit'
+                // }
+            },
+            tooltip: {
+                pointFormat: '<b>{point.percentage:.2f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    startAngle: -35,
+                    colors: [expenseColor, incomeColor],
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b><br/> R$ {point.y:.2f}'
+                    }
+                }
+            },
+            series: [{
+                name: 'Transações',
+                innerSize: '80%',
+                data: this.getData(0, 0)
+            }]
+        };
+
+        return (
+            <div>
+                <ReactHighcharts neverReflow={true} config={config} ref="chart" />
+            </div>
+        );
+    }
+}
 
 export default TransactionsDonutChart;
