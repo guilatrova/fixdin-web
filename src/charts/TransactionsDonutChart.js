9bb530b66d155f20d3442ac938c952a52a01b001
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-10-18 20:36:08 -0300

    Added drilldown with sample data

--- src/charts/TransactionsDonutChart.js
+++ src/charts/TransactionsDonutChart.js
@@ -4,8 +4,14 @@ import PropTypes from 'prop-types';
 import purple from '@material-ui/core/colors/purple';
 import blue from '@material-ui/core/colors/blue';
 import ReactHighcharts from 'react-highcharts';
+import drilldown from 'highcharts/modules/drilldown';
 
 import { formatCurrencyDisplay } from '../utils/formatters';
+
+drilldown(ReactHighcharts.Highcharts);
+ReactHighcharts.Highcharts.setOptions({
+    lang: { drillUpText: "Voltar" }
+});
 
 const expenseColor = blue["300"];
 const incomeColor = purple['500'];
@@ -32,17 +38,33 @@ class TransactionsDonutChart extends React.Component {
         chart.title.update({ text: totalDisplay });
     }
 
+    setTitle = (text = "") => {
+        const chart = this.refs.chart.getChart();
+        chart.title.update({ text });
+    }
+
     getData = (incomes, expenses) => (
         [
             {
                 name: 'Despesas',
-                y: -expenses
+                y: -expenses,
+                drilldown: "Chrome"
             }, {
                 name: 'Receitas',
-                y: incomes
+                y: incomes,
+                drilldown: "Firefox"
             }
         ]
     );
+
+    handleDrilldown = () => {
+        this.setTitle();
+    }
+
+    handleDrillup = () => {
+        const totalDisplay = formatCurrencyDisplay(this.props.total);
+        this.setTitle(totalDisplay);
+    }
 
     render() {
         const config = {
@@ -54,6 +76,10 @@ class TransactionsDonutChart extends React.Component {
                 height: 220,
                 animation: {
                     duration: duration
+                },
+                events: {
+                    drilldown: this.handleDrilldown,
+                    drillup: this.handleDrillup
                 }
             },
             credits: false,
@@ -71,12 +97,11 @@ class TransactionsDonutChart extends React.Component {
             },
             plotOptions: {
                 pie: {
-                    allowPointSelect: true,
                     cursor: 'pointer',
                     colors: [expenseColor, incomeColor],
                     dataLabels: {
                         enabled: true,
-                        format: '<b>{point.name}</b><br/> R$ {point.y:.2f}'
+                        format: '<b>{point.name}</b><br/> R$ {point.y:.2f}',
                     }
                 }
             },
@@ -84,7 +109,43 @@ class TransactionsDonutChart extends React.Component {
                 name: 'Transações',
                 innerSize: '80%',
                 data: this.getData(0, 0)
-            }]
+            }],
+            drilldown: {
+                activeDataLabelStyle: {
+                    textDecoration: 'none',
+                    color: 'black'
+                },
+                series: [
+                    {
+                        name: "Chrome",
+                        id: "Chrome",
+                        data: [
+                            [
+                                "v65.0",
+                                0.1
+                            ],
+                            [
+                                "v64.0",
+                                1.3
+                            ],
+                        ]
+                    },
+                    {
+                        name: "Firefox",
+                        id: "Firefox",
+                        data: [
+                            [
+                                "v65.0",
+                                0.1
+                            ],
+                            [
+                                "v64.0",
+                                1.3
+                            ],
+                        ]
+                    }
+                ]
+            }
         };
 
         return (
