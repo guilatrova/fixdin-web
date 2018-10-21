7066f8cb5369e9fc0f02cfa8664dc0d2b06841da
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-10-21 14:28:01 -0300

    Added isStartupAccount specification

--- src/transactions/transactions/consts.js
+++ src/transactions/transactions/consts.js
@@ -1,3 +1,4 @@
 //Those consts below should match our API BoundReasons
 export const PERIODIC_REASON = "PERIODIC";
 export const TRANSFER_REASON = "ACCOUNT_TRANSFER";
+export const STARTUP_ACCOUNT_DESCRIPTION = "STARTUP_ACCOUNT";
--- src/transactions/transactions/specifications.js
+++ src/transactions/transactions/specifications.js
@@ -1,13 +1,15 @@
 import moment from 'moment';
 
-import { PERIODIC_REASON, TRANSFER_REASON } from './consts';
+import { PERIODIC_REASON, TRANSFER_REASON, STARTUP_ACCOUNT_DESCRIPTION } from './consts';
 
 const isPeriodic = transaction => (transaction.bound_reason === PERIODIC_REASON);
 const isTransfer = transaction => (transaction.bound_reason === TRANSFER_REASON);
+const isStartupAccount = transaction => (transaction.description === STARTUP_ACCOUNT_DESCRIPTION);
 const isOverdue = transaction => !transaction.payment_date && moment().isAfter(transaction.due_date);
 
 export default {
     isPeriodic,
     isTransfer,
-    isOverdue
+    isOverdue,
+    isStartupAccount
 };
