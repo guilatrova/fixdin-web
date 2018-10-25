64eb05683194ef3733af7e6f993ccb8fc8a8c035
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-10-25 19:06:19 -0300

    Added mini prop for AccountAvatar

--- src/transactions/accounts/components/AccountAvatar.js
+++ src/transactions/accounts/components/AccountAvatar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
+import cn from 'classnames';
 import NotInterestedIcon from '@material-ui/icons/NotInterested';
 import Avatar from '@material-ui/core/Avatar';
 
@@ -12,12 +13,20 @@ const styles = {
     },
     avatar: {
         marginRight: 10
+    },
+    miniAvatar: {
+        width: 20,
+        height: 20
     }
 };
 
-const AccountAvatar = ({ classes, account }) => (
+const AccountAvatar = ({ classes, account, mini }) => (
     <div className={classes.root}>
-        <Avatar alt="account-avatar" src={account.avatar} className={classes.avatar}>
+        <Avatar
+            alt="account-avatar"
+            src={account.avatar}
+            className={cn(classes.avatar, { [classes.miniAvatar]: mini })}
+        >
             {!account.avatar && <NotInterestedIcon />}
         </Avatar>
         {account.name}
@@ -26,7 +35,8 @@ const AccountAvatar = ({ classes, account }) => (
 
 AccountAvatar.propTypes = {
     classes: PropTypes.object.isRequired,
-    account: PropTypes.object.isRequired
+    account: PropTypes.object.isRequired,
+    mini: PropTypes.bool
 };
 
 export default withStyles(styles)(AccountAvatar);
